4f3a9545d4f9edb8bbed947c31c5252bf038e4e8
asice <2492215094@qq.com>
2025-01-19 02:29:57 +0800

    feat:although I haven't achieved all, just this today

--- algo_test.go
+++ algo_test.go
@@ -1,1 +1,18 @@
 package main
+
+import "testing"
+
+func Test_al(t *testing.T) {
+	t.Log("algo_test.go")
+	t.Log(B58Encode([]byte("hello")))
+	t.Log(B58Decode([]byte("hello")))
+}
+
+func Bench_al(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		B58Encode([]byte("hello"))
+	}
+	for i := 0; i < b.N; i++ {
+		B58Decode([]byte("hello"))
+	}
+}
